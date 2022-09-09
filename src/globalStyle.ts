c880f4515d6fcb56a4d94057c9283da33d14b2da
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-09-09 11:27:11 -0300

    style: light theme colors added

--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -39,9 +39,25 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   @media (prefers-color-scheme: light) {
+    :root {
+    --white: #fff;
+    --gray-900: #e1e1e6;
+    --gray-800: #c4c4cc;
+    --gray-700: #8d8d99;
+    --gray-600: #323238;
+    --gray-400: #29292e;
+    --gray-300: #202024;
+    --gray-100: #121214;
+    --purple-900: #B08FFF;
+    --purple-700: #9F75FF;
+    --purple-500: #8257E5;
+    --purple-300: #6F48C9;
+    --purple-100: #5E3FA6;
+  }
+  
     body {
-      background: var(--gray-100);
-      color: var(--gray-900);
+      background: var(--gray-900);
+      color: var(--gray-300);
     }
   }
 `;
