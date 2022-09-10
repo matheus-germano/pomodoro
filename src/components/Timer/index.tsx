7b3f30922946f3224e4e266d4f3069189009d845
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-09-10 07:55:05 -0300

    perf: timeout modifications to improve precision

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,50 +1,60 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { VscDebugRestart } from 'react-icons/vsc';
 import { IoPlay } from 'react-icons/io5';
 import { BsFillPauseFill } from 'react-icons/bs';
 
 import { TimerContainer, TimerWrapper, TimerDisplay, TimerControllers } from './styles';
 
-const INITIAL_TIMER_SECONDS = 25 * 60; //25 minutes
+const INITIAL_TIMER_SECONDS = 1 * 60; //25 minutes
 const REST_TIMER_SECONDS = 5 * 60; //5 minutes
 
 export function Timer() {
-  const [secondsAmount, setSecondsAmount] = useState(INITIAL_TIMER_SECONDS);
+  const [secondsAmount, setSecondsAmount] = useState(5);
   const [isRestTime, setIsRestTime] = useState(false);
-  const [isTimerPaused, setIsTimerPaused] = useState(false);
+  const [isTimerPaused, setIsTimerPaused] = useState(true);
+
+  const isTimerPausedRef = useRef(isTimerPaused);
+  const isRestTimeRef = useRef(isRestTime);
 
   let minutes = Math.floor(secondsAmount / 60);
   let seconds = secondsAmount % 60;
 
   useEffect(() => {
-    if (isTimerPaused) {
-      return;
-    }
+    Notification.requestPermission();
+  }, []);
 
-    if (secondsAmount > 0) {
-      setTimeout(() => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (isTimerPausedRef.current) {
+        return;
+      }
+
+      if (secondsAmount > 0) {
         setSecondsAmount(prev => prev - 1);
-      }, 1000);
-    } else if (isRestTime) {
-      setIsRestTime(false);
-      setSecondsAmount(INITIAL_TIMER_SECONDS);
-    } else {
-      setIsRestTime(true);
-      setSecondsAmount(REST_TIMER_SECONDS);
-    }
-  }, [secondsAmount, isTimerPaused]);
+      } else if (isRestTimeRef.current) {
+        setIsRestTime(false);
+        setSecondsAmount(INITIAL_TIMER_SECONDS);
+      } else {
+        setIsRestTime(true);
+        setSecondsAmount(REST_TIMER_SECONDS);
+      }
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [secondsAmount, isTimerPausedRef.current]);
 
   function handleRestartTimer() {
     setSecondsAmount(INITIAL_TIMER_SECONDS);
   }
 
   function handlePauseTimer() {
-    setIsTimerPaused(!isTimerPaused);
+    isTimerPausedRef.current = !isTimerPausedRef.current;
+    setIsTimerPaused(isTimerPausedRef.current);
   }
 
   return (
     <TimerContainer>
-      <h2>{isRestTime ? "Let's rest! 😮‍💨🍃" : "Let's work! 😄🚀"}</h2>
+      <h2>{isRestTimeRef.current ? "Let's rest! 😮‍💨🍃" : "Let's work! 😄🚀"}</h2>
       <TimerWrapper>
         <TimerDisplay>
           <span>{String(minutes).padStart(2, '0')}</span>
@@ -52,7 +62,7 @@ export function Timer() {
           <span>{String(seconds).padStart(2, '0')}</span>
         </TimerDisplay>
         <TimerControllers>
-          <button onClick={handlePauseTimer}>{isTimerPaused ? <IoPlay size={20} /> : <BsFillPauseFill size={20} />}</button>
+          <button onClick={handlePauseTimer}>{isTimerPausedRef.current ? <IoPlay size={20} /> : <BsFillPauseFill size={20} />}</button>
           <button onClick={handleRestartTimer}><VscDebugRestart size={20} /></button>
         </TimerControllers>
       </TimerWrapper>
