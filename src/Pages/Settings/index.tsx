2fc5887612401830f40598aee1a76b7fbe297dc8
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-09-12 17:25:12 -0300

    feat: Settings page in progress

--- src/Pages/Settings/index.tsx
+++ src/Pages/Settings/index.tsx
@@ -1,5 +1,33 @@
+import { BsArrowLeftShort } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { RangeInput } from "../../components/RangeInput";
+
+import { SettingsContainer, PageTitle, TimeChanger } from "./styles";
+
 export function Settings() {
+  const navigate = useNavigate();
   return (
-    <h1>Settings</h1>
+    <SettingsContainer>
+      <PageTitle>
+        <BsArrowLeftShort size={32} onClick={() => navigate("/")} className="back-button" />
+        <h2>Timer Settings</h2>
+      </PageTitle>
+      <TimeChanger>
+        <p>Change your work time!</p>
+        <RangeInput
+          min={5}
+          max={60}
+          step={1}
+        />
+      </TimeChanger>
+      <TimeChanger>
+        <p>Change your break time!</p>
+        <RangeInput
+          min={5}
+          max={60}
+          step={1}
+        />
+      </TimeChanger>
+    </SettingsContainer>
   );
 }
