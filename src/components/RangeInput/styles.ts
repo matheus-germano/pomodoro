460a61f90343edae9e2e31ab8e45a570fb59590b
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-09-12 17:53:43 -0300

    feat: Range Input done

--- src/components/RangeInput/styles.ts
+++ src/components/RangeInput/styles.ts
@@ -4,14 +4,9 @@ export const RangeInputWrapper = styled.input`
 
 &[type="range"] {
   -webkit-appearance: none;
-  margin-right: 15px;
-  width: 200px;
-  height: 7px;
-  background: rgba(255, 255, 255, 0.6);
-  border-radius: 5px;
-  background-image: linear-gradient(#ff4500, #ff4500);
-  background-size: 70% 100%;
-  background-repeat: no-repeat;
+  height: 15px;
+  background: var(--gray-800);
+  border-radius: 10px;
 }
 
 /* Input Thumb */
@@ -20,10 +15,13 @@ export const RangeInputWrapper = styled.input`
   height: 20px;
   width: 20px;
   border-radius: 50%;
-  background: #ff4500;
-  cursor: ew-resize;
-  box-shadow: 0 0 2px 0 #555;
+  background: var(--purple-500);
+  cursor: pointer;
   transition: background .3s ease-in-out;
+
+  &:hover {
+    background: var(--purple-700);
+  }
 }
 
 &[type="range"]::-moz-range-thumb {
@@ -31,10 +29,13 @@ export const RangeInputWrapper = styled.input`
   height: 20px;
   width: 20px;
   border-radius: 50%;
-  background: #ff4500;
-  cursor: ew-resize;
-  box-shadow: 0 0 2px 0 #555;
+  background: var(--purple-500);
+  cursor: pointer;
   transition: background .3s ease-in-out;
+
+  &:hover {
+    background: var(--purple-700);
+  }
 }
 
 &[type="range"]::-ms-thumb {
@@ -42,22 +43,13 @@ export const RangeInputWrapper = styled.input`
   height: 20px;
   width: 20px;
   border-radius: 50%;
-  background: #ff4500;
-  cursor: ew-resize;
-  box-shadow: 0 0 2px 0 #555;
+  background: var(--purple-500);
+  cursor: pointer;
   transition: background .3s ease-in-out;
-}
 
-&[type="range"]::-webkit-slider-thumb:hover {
-  background: #ff0200;
-}
-
-&[type="range"]::-moz-range-thumb:hover {
-  background: #ff0200;
-}
-
-&[type="range"]::-ms-thumb:hover {
-  background: #ff0200;
+  &:hover {
+    background: var(--purple-700);
+  }
 }
 
 /* Input Track */
